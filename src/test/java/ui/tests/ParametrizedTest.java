177d4f1f4f5edadb0c5ae0b8e0d8d91b6d51dc38
Mykola Sheremet <sheremetmykola@gmail.com>
2023-03-01 21:13:22 +0100

    data provider

---
+++ src/test/java/ui/tests/ParametrizedTest.java
@@ -0,0 +1,49 @@
+package ui.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ui.page.MenuEl;
+import ui.page.contact.AddContactWindow;
+import ui.page.contact.ContactPage;
+import ui.page.login.LoginPage;
+import util.DataProviders;
+
+import java.util.Map;
+
+public class ParametrizedTest extends TestBase {
+    LoginPage loginPage;
+    MenuEl menuEl;
+    ContactPage contactPage;
+
+    @BeforeMethod
+    public void precondition() {
+        loginPage = new LoginPage(driver);
+        loginPage.getLogin(EMAIL, PASSWORD);
+    }
+
+    @Test(dataProvider = "newContact", dataProviderClass = DataProviders.class)
+    public void addNewContactDataProviderTest(String firstName, String lastName, String about) {
+        menu = new MenuEl(driver);
+        menu.clickAddNewContactButton();
+        addContactWindow = new AddContactWindow(driver);
+        addContactWindow.fillData(firstName, lastName, about);
+        contactPage = new ContactPage(driver);
+        Map<String, String> info = contactPage.getInfo();
+        Assert.assertEquals(info.get("first name"), firstName);
+        Assert.assertEquals(info.get("last name"), lastName);
+        Assert.assertEquals(info.get("contact description"), about);
+    }
+
+    @Test(dataProvider = "newContactWithCsv", dataProviderClass = DataProviders.class)
+    public void addNewContactDataProviderCsvTest(String firstName, String lastName, String about) {
+        menu = new MenuEl(driver);
+        menu.clickAddNewContactButton();
+        addContactWindow = new AddContactWindow(driver);
+        addContactWindow.fillData(firstName, lastName, about);
+        contactPage = new ContactPage(driver);
+        Map<String, String> info = contactPage.getInfo();
+        Assert.assertEquals(info.get("first name"), firstName);
+        Assert.assertEquals(info.get("last name"), lastName);
+        Assert.assertEquals(info.get("contact description"), about);
+}
